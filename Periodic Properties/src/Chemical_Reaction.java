3b41742a4cb4c93f79efd96125f6d78bd38f9cca
Adish Betawar <abetawar@gmail.com>
2016-03-15 20:35:06 -0700

    new edits

--- Periodic Properties/src/Chemical_Reaction.java
+++ Periodic Properties/src/Chemical_Reaction.java
@@ -1,4 +1,11 @@
 
+/**
+ * fundamentals in balancing chemical reaction is that you must have 
+ * the same number of elements on the reactants side as the product
+ * @author Adish
+ *
+ */
+
 public class Chemical_Reaction 
 {
 	
@@ -6,10 +13,22 @@ public class Chemical_Reaction
 	{		
 		Object reactants;
 		Object products;
+		
 	}
 	
+	class Redox_Reaction
+	{
+		
+	}
 	
+	class Acid_Base_Reaction
+	{
+		
+	}
 	
-	
+	public void Identify_Reaction_Type()
+	{
+		
+	}
 
 }
